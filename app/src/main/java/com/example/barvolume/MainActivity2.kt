9ea803c29244af19e8f3f5ed82fbb8e4c24e3e1f
Your Name <you@example.com>
2020-10-05 16:21:14 +0800

    update Pertama

---
+++ app/src/main/java/com/example/barvolume/MainActivity2.kt
@@ -0,0 +1,39 @@
+package com.example.barvolume
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity2 : AppCompatActivity(), View.OnClickListener {
+    private lateinit var edtWidth: EditText
+    private lateinit var edtheight: EditText
+    private lateinit var edtLength: EditText
+    private lateinit var btnCalculate: Button
+    private lateinit var tvResult: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        edtWidth = findViewById(R.id.edt_width)
+        edtheight = findViewById(R.id.edt_height)
+        edtLength = findViewById(R.id.edt_length)
+        btnCalculate = findViewById(R.id.btn_calculate)
+        tvResult = findViewById(R.id.tv_result)
+
+        btnCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.btn_calculate) {
+            val inputLength = edtLength.text.toString().trim()
+            val inputWidth = edtWidth.text.toString().trim()
+            val inputHeight = edtheight.text.toString().trim()
+            val volume = inputLength.toDouble() * inputWidth.toDouble() * inputHeight.toDouble()
+            tvResult.text = volume.toString()
+        }
+    }
+}
